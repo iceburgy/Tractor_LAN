b232104b55f26391c07821e4b9d836b36503a934
iceburgy <iceburg2005@hotmail.com>
2021-09-15 18:48:55 -0700

    bump up version to 457

--- SourceCode/TractorWinformClient/Properties/AssemblyInfo.cs
+++ SourceCode/TractorWinformClient/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.456")]
-[assembly: AssemblyFileVersion("1.2.0.456")]
+[assembly: AssemblyVersion("1.2.0.457")]
+[assembly: AssemblyFileVersion("1.2.0.457")]
