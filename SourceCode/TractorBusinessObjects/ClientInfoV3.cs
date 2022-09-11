f5365fa7e1f910eedfdf08d8437b70484aca627b
iceburgy <iceburg2005@hotmail.com>
2022-09-11 11:09:19 -0700

    restrict max login history to 10

--- SourceCode/TractorBusinessObjects/ClientInfoV3.cs
+++ SourceCode/TractorBusinessObjects/ClientInfoV3.cs
@@ -22,11 +22,12 @@ namespace Duan.Xiugang.Tractor.Objects
         public List<string> loginHistory;
 
         private const string datePatt = @"yyyy/MM/dd-HH:mm:ss";
+        private const int maxLoginHistory = 10;
 
         public ClientInfoV3(string ip, string id, string orp, string pe)
         {
             PlayerID = id;
-            PlayerEmail= pe;
+            PlayerEmail = pe;
             overridePass = orp;
             playerIPList = new HashSet<string>();
             playerIPList.Add(ip);
@@ -39,11 +40,18 @@ namespace Duan.Xiugang.Tractor.Objects
             playerIPList.Add(ip);
             DateTime saveNow = DateTime.Now.ToLocalTime();
             string lastLogin = saveNow.ToString(datePatt);
+            purgeLoginHistory();
             loginHistory.Add(string.Format("{0} at {1}", ip, lastLogin));
             if (!string.IsNullOrEmpty(cheating))
             {
                 cheatHistory.Add(string.Format("{0} at {1}", cheating, lastLogin));
             }
+        }
+
+        private void purgeLoginHistory()
+        {
+            int purgeCount = this.loginHistory.Count - maxLoginHistory + 1;
+            if (purgeCount > 0) this.loginHistory.RemoveRange(0, purgeCount);
         }
     }
 }
