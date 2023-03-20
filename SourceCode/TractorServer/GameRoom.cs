b3ef15c41a01e0a9b115e6e0b314987d8d90b0de
iceburgy <iceburg2005@hotmail.com>
2023-03-19 21:54:29 -0700

    log new game players 3

--- SourceCode/TractorServer/GameRoom.cs
+++ SourceCode/TractorServer/GameRoom.cs
@@ -2113,8 +2113,8 @@ namespace TractorServer
             {
                 TractorHost.log.Debug(string.Format("新游戏即将开始，玩家【{0}】【{1}】对阵【{2}】【{3}】",
                     this.CurrentRoomState.CurrentGameState.Players[0].PlayerId,
-                    this.CurrentRoomState.CurrentGameState.Players[1].PlayerId,
                     this.CurrentRoomState.CurrentGameState.Players[2].PlayerId,
+                    this.CurrentRoomState.CurrentGameState.Players[1].PlayerId,
                     this.CurrentRoomState.CurrentGameState.Players[3].PlayerId));
                 //新游戏开始前给出提示信息，开始倒计时，并播放提示音，告诉玩家要抢庄
                 PublishMessage(new string[] { "新游戏即将开始", "做好准备抢庄！" });
