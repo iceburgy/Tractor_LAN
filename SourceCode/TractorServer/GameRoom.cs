c839d508d90b474d879317f86df3c1a47235acd9
iceburgy <iceburg2005@hotmail.com>
2023-12-19 06:19:50 -0800

    disable chat during a started game

--- SourceCode/TractorServer/GameRoom.cs
+++ SourceCode/TractorServer/GameRoom.cs
@@ -229,6 +229,12 @@ namespace TractorServer
                 CurrentRoomState.CurrentHandState.CurrentHandStep <= HandStep.Playing;
         }
 
+        public bool IsGameStarted()
+        {
+            return HandStep.DistributingCards <= CurrentRoomState.CurrentHandState.CurrentHandStep &&
+                CurrentRoomState.CurrentHandState.CurrentHandStep <= HandStep.Playing;
+        }
+
         private bool IsRoomFull()
         {
             return this.CurrentRoomState.CurrentGameState.Players.Where(p => p != null).Count() == 4;
